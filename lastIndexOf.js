1968df09af42f84fb3e45f750e74e7d63581d7c3
kir- <graysonclee@gmail.com>
2019-06-10 18:26:58 +0000

    last code

---
+++ lastIndexOf.js
@@ -0,0 +1,15 @@
+function lastIndexOf(array,findnum){
+  var position = -1;
+  array.forEach(function(x,y){
+    if(x === findnum){
+      position = y;
+    }
+    return;
+  });
+  return position;
+}
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
